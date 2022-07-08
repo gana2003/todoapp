c648034f9fe35176e10f4458f49c044c87c81be5
gana2003 <meenakshigana0@gmail.com>
2022-07-08 15:07:32 +0530

    service dvpd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
+import {httpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { TodosComponent } from './todos/todos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    httpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
